56aa49d8c3ac32ffe5b22c3252e6d2b2f075bacf
Kenrique _Ngwa <72971484+kenrique100@users.noreply.github.com>
2024-04-06 20:28:52 +0000

    Main code

--- time_series_visualizer.py
+++ time_series_visualizer.py
@@ -5,17 +5,22 @@ from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 
 # Import data (Make sure to parse dates. Consider setting index column to 'date'.)
-df = None
+df = pd.read_csv("fcc-forum-pageviews.csv", parse_dates=["date"], index_col="date")
 
 # Clean data
-df = None
+df = df[
+    (df["value"] >= df["value"].quantile(0.025)) &
+    (df["value"] <= df["value"].quantile(0.975))]
 
 
 def draw_line_plot():
     # Draw line plot
+    fig, ax = plt.subplots(figsize=(10, 5))
+    ax.plot(df.index, df["value"], color="red" linewidth=1)
 
-
-
+    ax.set_title("Daily freeCodeCamp Forum Page Views 5/2016-12/2019")
+    ax.set_xlabel("Date")
+    ax.set_ylabel("Page Views")
 
 
     # Save image and return fig (don't change this part)
@@ -27,7 +32,7 @@ def draw_bar_plot():
     df_bar = None
 
     # Draw bar plot
-
+    fig = plt.subplots()
 
 
 
